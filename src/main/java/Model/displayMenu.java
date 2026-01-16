5e0435337906e640e40c1319634f422f5f1a91ed
TienDung4529 <nguyentiendung05032006@gmail.com>
2026-01-16 22:41:42 +0700

    Add initial main menu display

---
+++ src/main/java/Model/displayMenu.java
@@ -0,0 +1,28 @@
+package Model;
+
+public class displayMenu {
+
+    // Menu chính
+    public static void displayMainMenu() {
+    }
+
+    // Menu quản lý nhân viên
+    public static void displayEmployeeMenu() {
+    }
+
+    // Menu quản lý chấm công
+    public static void displayAttendanceMenu() {
+    }
+
+    // Menu quản lý lương
+    public static void displaySalaryMenu() {
+    }
+
+    // Menu báo cáo
+    public static void displayReportMenu() {
+    }
+
+    // Menu chọn loại nhân viên
+    public static void displayEmployeeTypeMenu() {
+    }
+}
